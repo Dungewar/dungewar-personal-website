da038da2369ea437f1cbfde7e79543c947d37b0e
Dungewar <dungewar@gmail.com>
2025-08-22 09:38:52 -0700

    Do something

--- frontend/src/components/email-thingy.ts
+++ frontend/src/components/email-thingy.ts
@@ -1,15 +1,20 @@
-const form = document.querySelector('#formlol');
-import window from "../config/config.js";
-const url = window.CONFIG.API_URL;
+const form = document.querySelector<HTMLFormElement>('#email-submit-form');
+import { CONFIG } from "../config/config.ts";
+
+if (!form) {
+  throw new Error("Missing required form.");
+}
 
 form.onsubmit = e => {
   e.preventDefault();
+  // @ts-ignore
   console.log(`Submit ${e.target.email.value}`)
   // send POST to action
-  fetch(`${url}/api/email-list-subscribe`, {
+  fetch(`${CONFIG.API_URL}/api/email-list-subscribe`, {
     method: "POST",
     headers: {'content-type':'application/json'},
     body: JSON.stringify({
+      // @ts-ignore
       email: e.target.email.value,
     })
   });
