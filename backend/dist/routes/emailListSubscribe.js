d2d033a9554c0ebbb4742d7f1a630bc01f6c4dff
Dungewar <dungewar@gmail.com>
2025-08-24 11:15:08 -0700

    build: update dist

--- backend/dist/routes/emailListSubscribe.js
+++ backend/dist/routes/emailListSubscribe.js
@@ -6,21 +6,27 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.emailListSubscribe = void 0;
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
-const emailListSubscribe = (req, res) => {
-    logMessage(`${req.body.email}`);
-    res.status(200).send('Added to email list!');
-};
-exports.emailListSubscribe = emailListSubscribe;
 const logDir = "/srv/dungewar-personal-website-data/data/";
-const logFile = path_1.default.join(logDir, 'updateEmailSubscription.txt');
+const logFile = path_1.default.join(logDir, 'email-subscriptions-updates.txt');
 // Make sure the logs directory exists
 if (!fs_1.default.existsSync(logDir)) {
     fs_1.default.mkdirSync(logDir, { recursive: true });
 }
-// Append a log line
-function logMessage(message) {
+const emailListSubscribe = (req, res) => {
+    const existingEmails = fs_1.default.readFileSync(path_1.default.join(logDir, 'emailListSubscribe.json'), 'utf8');
+    const lines = existingEmails.split("\n");
+    for (const line of lines) {
+        const parts = line.trim().split(/\s+/);
+        if (parts[1].trim() === req.body.email.trim()) {
+            res.status(201).send('Email address already exists');
+            return;
+        }
+    }
     const timestamp = new Date().toISOString();
-    const logLine = `${timestamp} ${message}\n`;
+    const logLine = `${timestamp} ${req.body.email} ${req.ip}\n`;
     fs_1.default.appendFileSync(logFile, logLine, 'utf8');
-}
+    res.status(200).send('Added to email list!');
+    return;
+};
+exports.emailListSubscribe = emailListSubscribe;
 //# sourceMappingURL=emailListSubscribe.js.map
