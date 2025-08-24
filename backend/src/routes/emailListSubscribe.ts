c6103513ee4cd69d2cec4fbc6d797dc9d5a21bf6
Dungewar <dungewar@gmail.com>
2025-08-24 10:52:29 -0700

    Do something

--- backend/src/routes/emailListSubscribe.ts
+++ backend/src/routes/emailListSubscribe.ts
@@ -3,22 +3,35 @@ import fs from 'fs';
 import path from 'path';
 import os from 'os';
 
-export const emailListSubscribe = (req: Request, res: Response): void => {
-    logMessage(`${req.body.email}`);
-    res.status(200).send('Added to email list!');
-}
-
 const logDir = "/srv/dungewar-personal-website-data/data/";
-const logFile = path.join(logDir, 'updateEmailSubscription.txt');
+const logFile = path.join(logDir, 'email-subscriptions-updates.txt');
 
 // Make sure the logs directory exists
 if (!fs.existsSync(logDir)) {
     fs.mkdirSync(logDir, { recursive: true });
 }
 
-// Append a log line
-function logMessage(message: string) {
+export const emailListSubscribe = (req: Request, res: Response): void => {
+
+    const existingEmails = fs.readFileSync(path.join(logDir, 'emailListSubscribe.json'), 'utf8');
+
+    const lines = existingEmails.split("\n");
+    for (const line of lines) {
+        const parts = line.trim().split(/\s+/);
+        if(parts[1].trim() === req.body.email.trim()) {
+            res.status(201).send('Email address already exists');
+            return;
+        }
+    }
+
+
     const timestamp = new Date().toISOString();
-    const logLine = `${timestamp} ${message}\n`;
+    const logLine = `${timestamp} ${req.body.email} ${req.ip}\n`;
     fs.appendFileSync(logFile, logLine, 'utf8');
+
+    res.status(200).send('Added to email list!');
+    return;
 }
+
+
+
