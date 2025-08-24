9a1073b19861a16fd231797f6dc72f4dea0b01b9
Dungewar <dungewar@gmail.com>
2025-08-24 10:14:57 -0700

    Do something

--- frontend/src/components/email-thingy.ts
+++ frontend/src/components/email-thingy.ts
@@ -1,25 +1,70 @@
-const form = document.querySelector<HTMLFormElement>('#email-submit-form');
 import {BASE_URL} from "../utils/url-utils.js";
 
-if (!form) {
-    throw new Error("Missing required form.");
+class MailSignupWidget extends HTMLElement {
+    constructor() {
+        super();
+
+        // --- Dialog ---
+        const emailSubmitDialog = document.createElement("dialog");
+
+        // --- Mail Button ---
+        const mailButton = document.createElement("button");
+        mailButton.textContent = "Sign up to our mailing list!";
+        mailButton.addEventListener("click", () => {
+            const dialog = emailSubmitDialog;
+            if (dialog?.open) dialog.close();
+            else dialog?.showModal();
+        });
+
+        // Close button
+        const closeButton = document.createElement("button");
+        closeButton.textContent = "X";
+        closeButton.addEventListener("click", () => {
+            emailSubmitDialog.close();
+        });
+
+        // Dialog text
+        const emailDialogText = document.createElement("p");
+        emailDialogText.textContent =
+            "Add your email to our mailing list to get FREE updates whenever the website changes!";
+
+        // Form
+        const emailForm = document.createElement("form");
+        emailForm.method = "post";
+
+        emailForm.addEventListener("submit", (e) => {
+            e.preventDefault();
+            const target = e.target as HTMLFormElement;
+            const emailInput = target.elements.namedItem("email") as HTMLInputElement;
+            console.log(`Submit ${emailInput.value}`);
+            fetch(`${BASE_URL}/api/email-list-subscribe`, {
+                method: "POST",
+                headers: {"content-type": "application/json"},
+                body: JSON.stringify({email: emailInput.value}),
+            });
+        });
+
+        // Email input
+        const emailInput = document.createElement("input");
+        emailInput.type = "email";
+        emailInput.placeholder = "example@gmail.net";
+        emailInput.name = "email"; // so target.elements.namedItem("email") works
+
+        // -- Submit button
+        const submitButton = document.createElement("button");
+        submitButton.type = "submit";
+        submitButton.textContent = "Submit";
+
+        // -- Put input + button inside form
+        emailForm.append(emailInput, submitButton);
+
+        // -- Put all pieces inside dialog
+        emailSubmitDialog.append(closeButton, emailDialogText, emailForm);
+
+        // -- Finally, put button + dialog in the DOM
+        this.append(mailButton, emailSubmitDialog);
+
+    }
 }
 
-
-
-form.onsubmit = e => {
-    e.preventDefault();
-
-    const target = e.target as HTMLFormElement;
-    const emailInput = target.elements.namedItem("email") as HTMLInputElement;
-
-    console.log(`Submit ${emailInput.value}`)
-    // send POST to action
-    fetch(`${BASE_URL}/api/email-list-subscribe`, {
-        method: "POST",
-        headers: {'content-type': 'application/json'},
-        body: JSON.stringify({
-            email: emailInput.value,
-        })
-    });
-}
+customElements.define("mail-signup-widget", MailSignupWidget);
