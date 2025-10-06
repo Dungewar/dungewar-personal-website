bfc3e026bb01d823e9d3a32b765540f27a603ebb
Dungewar <dungewar@gmail.com>
2025-10-06 12:57:34 -0700

    Do something

--- backend/src/helpers/fileHandler.ts
+++ backend/src/helpers/fileHandler.ts
@@ -5,9 +5,15 @@ const LOG_DIR = '/srv/dungewar-personal-website-data/website-data/logs/';
 const DATA_DIR = '/srv/dungewar-personal-website-data/website-data/data/';
 fs.mkdirSync(path.dirname(LOG_DIR), {recursive: true});
 
-const timestamp = () => {return new Date().toISOString()};
-const logPrefix = `[MSG] [${timestamp}] `;
-const errorPrefix = `[ERR] [${timestamp}] `;
+const timestamp = () => {
+    return new Date().toISOString()
+};
+const logPrefix = () => {
+    `[MSG] [${timestamp}] `
+};
+const errorPrefix = () => {
+    `[ERR] [${timestamp}] `
+};
 
 export function logMessageFile(file: string, msg: string) {
     const LOG_FILE = path.join(LOG_DIR, file);
@@ -16,6 +22,7 @@ export function logMessageFile(file: string, msg: string) {
     console.log(msg);
     fs.appendFileSync(LOG_FILE, msg);
 }
+
 export function logErrorFile(file: string, msg: string) {
     const LOG_FILE = path.join(LOG_DIR, file);
     msg = `${errorPrefix}${msg}\n`;
@@ -23,11 +30,13 @@ export function logErrorFile(file: string, msg: string) {
     console.error(msg);
     fs.appendFileSync(LOG_FILE, msg);
 }
+
 export function logMessage(msg: string) {
     msg = `${logPrefix}${msg}`;
 
     console.log(msg)
 }
+
 export function logError(msg: string) {
     msg = `${errorPrefix}${msg}`;
 
