77d43eaef57b93e94daac67b770e9fca23d64430
Dungewar <dungewar@gmail.com>
2025-11-12 22:18:53 -0800

    Do something

---
+++ backend/src/routes/webSocketCommunication.ts
@@ -0,0 +1,32 @@
+import WebSocket from 'ws';
+import {appendDataFile, readDataFile} from "../helpers/fileHandler";
+import {parse} from "node:url";
+
+export const webSocketHandler = (socket: WebSocket) => {
+
+
+    console.log(`Websocket connection started`);
+
+    socket.send(JSON.stringify({
+        "message": `Websocket connection started, fick you`
+    }));
+    socket.on('message', (message) => {
+        console.log(`Client says ${message}`);
+        const parsedMessage = JSON.parse(message.toString());
+
+        if(parsedMessage.message)
+            appendDataFile("chatroom_messages.txt", parsedMessage.message);
+
+        const messages = readDataFile("chatroom_messages.txt");
+
+        socket.send(JSON.stringify({
+            "message": messages
+        }));
+    })
+    socket.on('error', (err) => {
+        console.error(`Web socket error: ${err}`);
+    })
+    socket.on('close', () => {
+        console.log('Client disconnected');
+    })
+};
