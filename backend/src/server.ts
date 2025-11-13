97f388319fcc92977541ddf8be34fe0ab3bd0af1
Dungewar <dungewar@gmail.com>
2025-11-12 19:59:50 -0800

    Do something

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,6 +6,7 @@ import cors from "cors";
 
 const app = express();
 const PORT = 4000;
+const webSocket = new WebSocket('ws://localhost:8080');
 
 app.use(express.json());
 app.use(cors());
@@ -17,8 +18,27 @@ app.get('/health', (_req, res) => {
     res.send('ok');
 });
 
-
 app.listen(PORT, () => {
     console.log(`Backend listening on http://localhost:${PORT}`);
     console.log(`Also hello, this is nothing you should worry about :)`);
+});
+
+webSocket.addEventListener('open', () => {
+    console.log("WebSocket is connected");
+
+    webSocket.send('Hello, server')
+});
+
+webSocket.addEventListener('message', event => {
+    console.log("Received message: ", event.data);
+});
+
+// Executes when the connection is closed, providing the close code and reason.
+webSocket.addEventListener('close', event => {
+    console.log('WebSocket connection closed:', event.code, event.reason);
+});
+
+// Executes if an error occurs during the WebSocket communication.
+webSocket.addEventListener('error', error => {
+    console.error('WebSocket error:', error);
 });
