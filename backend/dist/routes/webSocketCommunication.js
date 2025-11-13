67a0506a513e685838d140f86c7c5792fc61c815
Dungewar <dungewar@gmail.com>
2025-11-13 15:20:26 -0800

    build: update dist

--- backend/dist/routes/webSocketCommunication.js
+++ backend/dist/routes/webSocketCommunication.js
@@ -2,8 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.webSocketHandler = void 0;
 const fileHandler_1 = require("../helpers/fileHandler");
+const server_1 = require("../server");
+// let listenerList: WebSocket[] = [];
 const webSocketHandler = (socket) => {
     console.log(`Websocket connection started`);
+    // listenerList.push(socket);
     {
         const messages = (0, fileHandler_1.readDataFile)("chatroom_messages.txt", 10);
         socket.send(JSON.stringify({
@@ -16,10 +19,13 @@ const webSocketHandler = (socket) => {
             const parsedMessage = JSON.parse(message.toString());
             if (parsedMessage.message)
                 (0, fileHandler_1.appendDataFile)("chatroom_messages.txt", `[${new Date().toISOString()}] ${parsedMessage.message}`);
+            // send updates to all the sockets
             const messages = (0, fileHandler_1.readDataFile)("chatroom_messages.txt", 10);
-            socket.send(JSON.stringify({
-                "message": messages
-            }));
+            server_1.webSocketServer.clients.forEach((client) => {
+                client.send(JSON.stringify({
+                    "message": messages
+                }));
+            });
         }
         catch (error) {
             console.error(error);
@@ -27,6 +33,7 @@ const webSocketHandler = (socket) => {
     });
     socket.on('error', (err) => {
         console.error(`Web socket error: ${err}`);
+        // listenerList.;
     });
     socket.on('close', () => {
         console.log('Client disconnected');
--- backend/dist/server.js
+++ backend/dist/server.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.webSocketServer = void 0;
 const express_1 = __importDefault(require("express"));
 const pullWebhook_1 = require("./routes/pullWebhook");
 const miscLogger_1 = require("./routes/miscLogger");
@@ -28,6 +29,7 @@ app.get('/api/health', (_req, res) => {
 app.listen(BACKEND_PORT, () => {
     (0, fileHandler_1.logMessage)(`Backend listening on http://localhost:${BACKEND_PORT}`);
 });
-const webSocketServer = new ws_1.WebSocketServer({ port: WEBSOCKET_PORT });
-webSocketServer.on('connection', webSocketCommunication_1.webSocketHandler);
+exports.webSocketServer = new ws_1.WebSocketServer({ port: WEBSOCKET_PORT });
+// webSocketServer.clients
+exports.webSocketServer.on('connection', webSocketCommunication_1.webSocketHandler);
 //# sourceMappingURL=server.js.map
