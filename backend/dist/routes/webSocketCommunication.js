165277ecbdd2a2c1028d3707a76e5f16fedd1aee
Dungewar <dungewar@gmail.com>
2025-11-13 15:38:17 -0800

    build: update dist

--- backend/dist/routes/webSocketCommunication.js
+++ backend/dist/routes/webSocketCommunication.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.webSocketHandler = void 0;
 const fileHandler_1 = require("../helpers/fileHandler");
 const server_1 = require("../server");
+const numberManipulation_1 = require("../helpers/numberManipulation");
 // let listenerList: WebSocket[] = [];
 const webSocketHandler = (socket) => {
     console.log(`Websocket connection started`);
@@ -18,9 +19,12 @@ const webSocketHandler = (socket) => {
         try {
             const parsedMessage = JSON.parse(message.toString());
             if (parsedMessage.message)
-                (0, fileHandler_1.appendDataFile)("chatroom_messages.txt", `[${new Date().toISOString()}] ${parsedMessage.message}`);
+                (0, fileHandler_1.appendDataFile)("chatroom_messages.txt", `[${new Date().toLocaleTimeString()}] ${parsedMessage.message}`);
+            let lines = 15;
+            if (parsedMessage.lines)
+                lines = (0, numberManipulation_1.clamp)(lines, 1, 30);
             // send updates to all the sockets
-            const messages = (0, fileHandler_1.readDataFile)("chatroom_messages.txt", 10);
+            const messages = (0, fileHandler_1.readDataFile)("chatroom_messages.txt", lines);
             server_1.webSocketServer.clients.forEach((client) => {
                 client.send(JSON.stringify({
                     "message": messages
