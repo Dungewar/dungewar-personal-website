bc46b1bc3fe8b511ca0893d4bb0aabb3f96311f5
Dungewar <dungewar@gmail.com>
2025-11-12 20:03:58 -0800

    build: update dist

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -12,6 +12,7 @@ const ringBuzzer_1 = require("./routes/ringBuzzer");
 const fileHandler_1 = require("./helpers/fileHandler");
 const app = (0, express_1.default)();
 const PORT = 4000;
+const webSocket = new WebSocket('ws://localhost:8080');
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 app.post('/api/webhook', pullWebhook_1.webhookHandler);
@@ -24,5 +25,20 @@ app.get('/api/health', (_req, res) => {
 });
 app.listen(PORT, () => {
     (0, fileHandler_1.logMessage)(`Backend listening on http://localhost:${PORT}`);
+});
+webSocket.addEventListener('open', () => {
+    console.log("WebSocket is connected");
+    webSocket.send('Hello, server');
+});
+webSocket.addEventListener('message', event => {
+    console.log("Received message: ", event.data);
+});
+// Executes when the connection is closed, providing the close code and reason.
+webSocket.addEventListener('close', event => {
+    console.log('WebSocket connection closed:', event.code, event.reason);
+});
+// Executes if an error occurs during the WebSocket communication.
+webSocket.addEventListener('error', error => {
+    console.error('WebSocket error:', error);
 });
 //# sourceMappingURL=server.js.map
