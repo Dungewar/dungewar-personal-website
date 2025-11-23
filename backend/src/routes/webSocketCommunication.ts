a53710a185310cc076c9c4e30e842165e8aff7dc
Dungewar <dungewar@gmail.com>
2025-11-22 19:07:00 -0800

    Do something

--- backend/src/routes/webSocketCommunication.ts
+++ backend/src/routes/webSocketCommunication.ts
@@ -5,22 +5,29 @@ import { clamp } from "../helpers/numberManipulation";
 import { addMessage, getMessage as getMessages, MessageRow } from '../helpers/databaseHandler';
 
 const messageCount = 15;
+const messageDelay = 3;
 
 export const webSocketHandler = (socket: WebSocket) => {
     console.log(`Websocket connection started`);
+    let lastSent = 0;
 
     socket.send(JSON.stringify({
         "messages": getMessages(messageCount)
     }));
 
     socket.on('message', (message) => {
+
+        if (new Date().getUTCMilliseconds() - lastSent < messageDelay * 1000)
+            return; // they're spamming
+
+        lastSent = new Date().getUTCMilliseconds();
+
         console.log(`Client says ${message.toString()}`);
         try {
             const parsedMessage = JSON.parse(message.toString());
 
             if (parsedMessage.message)
                 addMessage("Anonymous", parsedMessage.message);
-
 
             // if (parsedMessage.messageCount)
             //     messageCount = clamp(messageCount, 1, 30);
