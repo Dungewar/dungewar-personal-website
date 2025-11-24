e96416fe8a253d2b7b19dbf3f4fec3d6bdf50991
Dungewar <dungewar@gmail.com>
2025-11-24 13:40:12 -0800

    Do something

--- backend/src/helpers/aiHandler.ts
+++ backend/src/helpers/aiHandler.ts
@@ -4,7 +4,8 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 export async function askAI(prompt: string) {
-  console.log("Asking Gemini: ", prompt, "\nAPI Key: ", genAI);
+  return null;
+  // console.log("Asking Gemini: ", prompt, "\nAPI Key: ", genAI);
   const result = await model.generateContent(prompt);
   console.log("Response received: ", result.response.text());
   return result;
