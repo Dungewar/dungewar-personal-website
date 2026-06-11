c90e5d957edc0fd656783d593bff0f67657db5f2
Olgierd Matusiewicz <dungewar@gmail.com>
2026-06-10 21:10:40 -0700

    Transform frontend to react

--- frontend/dist/utils/url-utils.js
+++
@@ -1,6 +0,0 @@
-let { protocol, hostname, port } = window.location;
-if (port === "63342") {
-    port = "4000";
-}
-export const BASE_URL = `${protocol}//${hostname}:${port}`;
-//# sourceMappingURL=url-utils.js.map
