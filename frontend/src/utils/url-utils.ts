62577c29e0f94309767913279352e3b18cb3ef39
Dungewar <dungewar@gmail.com>
2026-09-05 15:46:05 -0700

    Switch to REACT

--- frontend/src/utils/url-utils.ts
+++
@@ -1,7 +0,0 @@
-let {protocol, hostname, port} = window.location;
-
-if (port === "63342") {
-    port = "4000";
-}
-
-export const BASE_URL: string = `${protocol}//${hostname}:${port}`;
--- update-project-entry.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /srv/dungewar-personal-website || exit
-git stash
-git pull
-./update-project.sh
